9cff86f5551ac6f6926033dbc10528c82de47a32
Thanh Long <tranthanhlong.q9@gmail.com>
2021-05-13 18:21:32 +0700

    upload TPS models

---
+++ Assets/My Scripts/GrimAnimator.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrimAnimator : MonoBehaviour
+{
+    #region Variable.
+    [Header("Animator")]
+    [SerializeField] private Animator grimAnimator;
+
+    private float vertical = 0.0f;
+    private float horizontal = 0.0f;
+
+    private bool isPistol = false;
+    private bool isSprint = false;
+    private bool isAiming = false;
+    private bool isFiring = false;
+    #endregion
+
+    #region Get Functions.
+    public float GetVertical()
+    {
+        return vertical;
+    }
+    public float GetHorizontal()
+    {
+        return horizontal;
+    }
+    public bool GetIsPistol()
+    {
+        return isPistol;
+    }
+    public bool GetIsSprint()
+    {
+        return isSprint;
+    }
+    public bool GetIsAiming()
+    {
+        return isAiming;
+    }
+    public bool GetIsFiring()
+    {
+        return isFiring;
+    }
+
+    #endregion
+
+    #region Functions.
+    private void Update()
+    {
+        Moving();
+    }
+
+    private void Moving()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+        
+
+        grimAnimator.SetFloat("Vertical", vertical);
+        grimAnimator.SetFloat("Horizontal", horizontal);
+    }
+    #endregion
+}
---
+++ Assets/My Scripts/GrimMoments.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrimMoments : MonoBehaviour
+{
+    #region Variable.
+    [Header("Grim Animator")]
+    [SerializeField] private GrimAnimator grimAnimator;
+
+    [SerializeField]private float speed = 2.0f;
+    private float sensivity = 1.0f;
+    private float damping = 0.5f;
+
+    private Vector2 V2Move = Vector2.zero;
+    private Vector2 V2Mouse = Vector2.zero;
+
+    private Vector3 V3DirectionMove = Vector3.zero;
+    private Vector3 V3CurrentRotations = Vector3.zero;
+    private Vector3 V3DirectionRotation = Vector3.zero;
+
+    #endregion
+
+    #region GetFunctions.
+    public float GetSensivity()
+    {
+        return sensivity;
+    }
+
+    #endregion
+
+    #region Functions.
+    public void Update()
+    {
+        Turning();
+        Moving();
+    }
+
+    private void Turning()
+    {
+        V2Mouse = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+
+        V3CurrentRotations.x = Mathf.Lerp(V3CurrentRotations.x, V2Mouse.x, 1.0f / damping);
+        V3DirectionRotation = Vector3.up * V3CurrentRotations.x * sensivity;
+        transform.Rotate(V3DirectionRotation);
+    }
+    private void Moving()
+    {
+        V2Move = new Vector2(grimAnimator.GetVertical() * speed, grimAnimator.GetHorizontal()* speed);
+        V3DirectionMove = transform.forward * V2Move.x * Time.deltaTime + transform.right * V2Move.y * Time.deltaTime;
+        transform.position += V3DirectionMove;
+    }
+
+    
+    #endregion
+}
